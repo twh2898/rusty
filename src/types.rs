800018cb9ce54b734cc293e6e2614813766a0887
Thomas <twh2898@vt.edu>
2016-07-16 02:49:06 -0400

    Added types.rs

---
+++ src/types.rs
@@ -0,0 +1,10 @@
+
+#[allow(unused_variables)]
+pub fn demo() {
+    let a = 'c';    //char variable
+    let b: i32 = 0;         //defines integer 32 (32 bits)
+    let c: bool = false;    //boolean variable
+    let d: &'static str = "Hello World"; //String or &'static str
+    let e = [1, 2, 3];      //array
+    let f = [0; 20];        //create a 20 size array and init all to 0
+}
